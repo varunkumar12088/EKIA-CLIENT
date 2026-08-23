8404b8af21f57f834e41262154f0c318fc9b66c2
Varun Kumar <varun.kumar12aug@gmail.com>
2026-08-23 21:51:31 +0530

    First commit

---
+++ src/main/java/com/learning/ekia/EkiaClientApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ekia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkiaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkiaClientApplication.class, args);
+	}
+
+}
